39cbdbe2533a36830e70b880bc51bc8cdbf94996
mikmak29 <mjogamaapple@gmail.com>
2025-08-10 22:04:29 +0800

    fix file directory

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,12 @@
 import { menuButton } from "./htmlElements/button.js";
 import { dropdown_list } from "./htmlElements/dropdown.js";
 
-const menuButtonSetup = () => {
+const menuButtonSetup = () => { 
     if (dropdown_list) {
         dropdown_list.classList.toggle('toggle-menu_button');
     }
 };
 
 menuButton.addEventListener('click', menuButtonSetup);
+
+
